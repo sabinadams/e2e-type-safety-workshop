53e40f08636fd53d3a35c6f0766129a0aa779b89
Sabin Adams <sabintheworld@gmail.com>
2022-11-21 12:49:53 -0800

    Fixes yoga

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,17 @@
-import { createServer } from '@graphql-yoga/node'
+import { createYoga } from '@graphql-yoga/node'
+import { createServer } from 'node:http'
 import { schema } from "./schema";
 import { prisma } from "./db";
 
-
-const server = createServer({
+const yoga = createYoga({
   schema,
   context: {
     prisma
   }
 })
 
-server.start()
+const server = createServer(yoga)
+
+server.listen(4000, () => {
+  console.info('Server is running on http://localhost:4000/graphql')
+})
